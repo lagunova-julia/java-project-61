97e98c2aeb412b574794aa8c4f369084346f6ee4
Julia Lagunova <lagunaajul@gmail.com>
2023-09-20 23:25:46 +0300

    add CheckStyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String greetingsUser(){
+    public static String greetingsUser() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("May I have your name? ");
